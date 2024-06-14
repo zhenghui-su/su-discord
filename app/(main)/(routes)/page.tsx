a15e7994c01e89e5b03a805c68df7d1885181051
苏征辉 <343196323@qq.com>
2024-06-14 22:52:11 +0800

    🎉 init: 初始化su-discord项目，引入shadcn-ui

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+	return (
+		<div>
+			<p className='text-3xl font-bold text-indigo-500'>Hello Discord</p>
+			<Button>Click me</Button>
+		</div>
+	)
+}
