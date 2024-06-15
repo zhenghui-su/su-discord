4eab7caea753c10984d00a829cb4034a6c4ad799
苏征辉 <343196323@qq.com>
2024-06-15 20:59:19 +0800

    ✨ feat: 新增主题切换功能

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
+
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,8 +19,17 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang='en'>
-				<body className={font.className}>{children}</body>
+			<html lang='en' suppressHydrationWarning>
+				<body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='dark'
+						enableSystem={false}
+						storageKey='discord-theme'
+					>
+						{children}
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	)
