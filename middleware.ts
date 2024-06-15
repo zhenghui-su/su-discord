bf39e8cb4ac2225de44e8ea22858fdccc4c79500
苏征辉 <343196323@qq.com>
2024-06-15 20:59:27 +0800

    📃 docs: 增加注释

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
+// 以前是 authMiddleware, 默认保护所有路线
+// 现在是 clerkMiddleware, 默认不保护所有路线, 需要手动添加保护路线
+// 下面是默认保护所有路线的代码
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware((auth, request) => {
