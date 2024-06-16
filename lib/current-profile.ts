17188281c7fe599e991e07ac205e57c4b6717fb5
苏征辉 <343196323@qq.com>
2024-06-16 22:47:54 +0800

    ✨ feat: 增加创建服务器功能

---
+++ lib/current-profile.ts
@@ -0,0 +1,20 @@
+import { auth } from "@clerk/nextjs/server"
+import { db } from "@/lib/db"
+/**
+ *
+ * @returns 返回当前用户信息
+ */
+export const currentProfile = async () => {
+	const { userId } = auth()
+
+	if (!userId) {
+		return null
+	}
+
+	const profile = await db.profile.findUnique({
+		where: {
+			userId,
+		},
+	})
+	return profile // 返回用户信息
+}
