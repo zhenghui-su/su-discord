b25026471e432332c37e101e32b44b225cab2365
苏征辉 <343196323@qq.com>
2024-06-16 22:24:44 +0800

    ✨ feat: 增加Modal上传图片和预览图片功能

---
+++ components/file-upload.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { X } from "lucide-react"
+import Image from "next/image"
+
+import { UploadDropzone } from "@/lib/uploadthing"
+
+import "@uploadthing/react/styles.css"
+
+interface FileUploadProps {
+	endpoint: "messageFile" | "serverImage"
+	value: string
+	onChange: (url?: string) => void
+}
+const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
+	const fileType = value?.split(".").pop()
+
+	if (value && fileType !== "pdf") {
+		return (
+			<div className='relative h-20 w-20'>
+				<Image fill src={value} alt='Upload' className='rounded-full' />
+				<button
+					onClick={() => onChange("")}
+					className='bg-rose-500 text-white p-1 rounded-full
+          absolute top-0 right-0 shadow-sm'
+					type='button'
+				>
+					<X className='h-4 w-4' />
+				</button>
+			</div>
+		)
+	}
+
+	return (
+		<UploadDropzone
+			endpoint={endpoint}
+			onClientUploadComplete={(res) => {
+				onChange(res?.[0].url)
+			}}
+			onUploadError={(error: Error) => {
+				console.log(error)
+			}}
+		/>
+	)
+}
+
+export default FileUpload
