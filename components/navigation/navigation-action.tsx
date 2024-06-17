16aa0fb11a4fe896f4726a9561ca468f5bf246bf
苏征辉 <343196323@qq.com>
2024-06-17 22:15:03 +0800

    ✨ feat: 新增侧边栏和服务器列表

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Plus } from "lucide-react"
+
+import { ActionTooltip } from "@/components/action-tooltip"
+/**
+ *
+ * @returns 侧边栏顶部-添加按钮
+ */
+export const NavigationAction = () => {
+	return (
+		<div>
+			<ActionTooltip label='Add a server' side='right' align='center'>
+				<button className='group flex items-center'>
+					<div
+						className='flex mx-3 h-[48px] w-[48px] rounded-[24px]
+          group-hover:rounded-[16px] transition-all overflow-hidden items-center
+          justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'
+					>
+						<Plus
+							className=' group-hover:text-white transition text-emerald-500'
+							size={25}
+						/>
+					</div>
+				</button>
+			</ActionTooltip>
+		</div>
+	)
+}
---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,71 @@
+import { redirect } from "next/navigation"
+
+import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { ModeToggle } from "@/components/mode-toggle"
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+
+import { NavigationAction } from "./navigation-action"
+import { NavigationItem } from "./navigation-item"
+import { UserButton } from "@clerk/nextjs"
+
+/**
+ *
+ * @returns 侧边栏
+ */
+export const NavigationSidebar = async () => {
+	const profile = await currentProfile()
+
+	if (!profile) {
+		return redirect("/")
+	}
+
+	const servers = await db.server.findMany({
+		where: {
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+	})
+
+	return (
+		<div
+			className='space-y-4 flex flex-col items-center
+    h-full text-primary w-full dark:bg-[#1E1F22] py-3'
+		>
+			{/* 头部添加按钮 */}
+			<NavigationAction />
+			{/* 分割线 */}
+			<Separator
+				className='h-[2px] bg-zinc-300 dark:bg-zinc-700
+				rounded-md w-10 mx-auto'
+			/>
+			{/* 服务器列表 */}
+			<ScrollArea className='flex-1 w-full'>
+				{servers.map((server) => (
+					<div key={server.id} className='mb-4'>
+						<NavigationItem
+							id={server.id}
+							imageUrl={server.imageUrl}
+							name={server.name}
+						/>
+					</div>
+				))}
+			</ScrollArea>
+			<div className='pb-3 mt-auto flex items-center flex-col gap-y-4'>
+				<ModeToggle />
+				<UserButton
+					afterSignOutUrl='/'
+					appearance={{
+						elements: {
+							avatarBox: "h-[40px] w-[40px]",
+						},
+					}}
+				/>
+			</div>
+		</div>
+	)
+}
