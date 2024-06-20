a76d20bce95072eb4034e544c529d5bb70b37864
苏征辉 <343196323@qq.com>
2024-06-20 18:11:54 +0800

    ✨ feat: 新增获取页面url的hooks

---
+++ hooks/use-origin.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+/**
+ *
+ * @returns 当前页面的 origin
+ */
+export const useOrigin = () => {
+	const [mounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	// 获取当前页面的 origin
+	const origin =
+		typeof window !== "undefined" && window.location.origin
+			? window.location.origin
+			: ""
+
+	if (!mounted) {
+		return ""
+	}
+	return origin
+}
