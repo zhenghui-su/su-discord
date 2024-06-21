54eb2c9cbe2e204b1fcd7508befad4258f500887
苏征辉 <343196323@qq.com>
2024-06-21 18:16:06 +0800

    ✨ feat: 新增管理员删除自己服务器的API

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -36,3 +36,41 @@ export async function PATCH(
 		return new Response("Internal Error", { status: 500 })
 	}
 }
+
+/**
+ * 用于管理员删除服务器的 API
+ *
+ *
+ * @param req
+ * @param param1 传入服务器Id
+ * @returns
+ */
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { serverId: string } }
+) {
+	try {
+		const profile = await currentProfile()
+
+		if (!profile) {
+			return new NextResponse("Unauthorized", { status: 401 })
+		}
+
+		if (!params.serverId) {
+			return new NextResponse("Server ID Missing", { status: 400 })
+		}
+
+		const server = await db.server.delete({
+			where: {
+				// 找对应服务器,当前个人资料的id和服务器管理员的id一致
+				id: params.serverId,
+				profileId: profile.id,
+			},
+		})
+
+		return NextResponse.json(server)
+	} catch (error) {
+		console.log("[SERVER_ID_DELETE]", error)
+		return new NextResponse("Internal Error", { status: 500 })
+	}
+}
