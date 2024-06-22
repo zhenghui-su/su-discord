8fb50248dca0d141ea89151f6a7136c55a4fffd5
苏征辉 <343196323@qq.com>
2024-06-23 02:15:51 +0800

    🎉 init: 待完成编辑消息API

---
+++ pages/api/socket/messages/[messageId].ts
@@ -0,0 +1,7 @@
+import { NextApiResponseServerIo } from "@/types"
+import { NextApiRequest } from "next"
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponseServerIo
+) {}
