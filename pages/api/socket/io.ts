60e9be2c50b2a17f3893f7bc9fd30a530cf4e7b9
苏征辉 <343196323@qq.com>
2024-06-22 23:02:00 +0800

    ✨ feat: 新增socket的服务端处理

---
+++ pages/api/socket/io.ts
@@ -0,0 +1,33 @@
+import { Server as NetServer } from "http"
+import { NextApiRequest } from "next"
+import { Server as ServerIO } from "socket.io"
+
+import { NextApiResponseServerIo } from "@/types"
+
+// 配置 API，禁用 bodyParser
+// 在 Next.js 中，API 路由默认启用了 bodyParser
+// 关闭后 Next.js 不会尝试解析请求体
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+}
+
+// 服务端的websocket
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+	if (!res.socket.server.io) {
+		// 检查是否已经存在 io 实例
+		const path = "/api/socket/io" // 定义 Socket.IO 的路径
+		const httpServer: NetServer = res.socket.server as any
+		const io = new ServerIO(httpServer, {
+			// 创建一个新的 Socket.IO 服务器实例
+			path: path,
+			// 保证路径没有尾部斜杠, 即不会出现 /api/socket/io/
+			addTrailingSlash: false,
+		})
+		res.socket.server.io = io // 将 io 实例附加到 res.socket.server
+	}
+	res.end() // 结束响应
+}
+
+export default ioHandler
--- types.ts
+++ types.ts
@@ -1,3 +1,6 @@
+import { Server as NetServer, Socket } from "net"
+import { NextApiResponse } from "next"
+import { Server as SocketIOServer } from "socket.io"
 import { Server, Member, Profile } from "@prisma/client"
 
 /**
@@ -8,4 +11,20 @@ import { Server, Member, Profile } from "@prisma/client"
  */
 export type ServerWithMembersWithProfiles = Server & {
 	members: (Member & { profile: Profile })[]
+}
+
+/**
+ * 用一个"socket"属性扩展"NextApiResponse"类型
+ * 该属性是一个带有"server"属性的"Socket"对象
+ * Socket对象同时扩展了"server"属性，该属性是一个带有"io"属性的"NetServer"对象。
+ * NetServer对象同时扩展了"io"属性，该属性是一个"SocketIOServer"对象。
+ *
+ * 用于表示WebSocket服务器res的类型
+ */
+export type NextApiResponseServerIo = NextApiResponse & {
+	socket: Socket & {
+		server: NetServer & {
+			io: SocketIOServer
+		}
+	}
 }
