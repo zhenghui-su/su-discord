489ab64ec8d1ce4404c03630136d1e27bf0eed56
苏征辉 <343196323@qq.com>
2024-06-22 17:18:11 +0800

    ✨ feat: 初始进入的页面重定向到通用频道页面

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,56 @@
-const ServerIdPage = () => {
-	return <div>Server ID Page</div>
+import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+interface ServerIdPageProps {
+	params: {
+		serverId: string
+	}
+}
+/**
+ * 初始进入的页面
+ * 1. 检查用户是否登录
+ * 2. 找到用户的服务器
+ * 3. 检查服务器是否包含通用频道 general
+ * 4. 重定向到通用频道 general
+ */
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+	const profile = await currentProfile()
+
+	if (!profile) {
+		return auth().redirectToSignIn()
+	}
+
+	// 找到对应的服务器
+	const server = await db.server.findUnique({
+		where: {
+			id: params.serverId,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+		include: {
+			channels: {
+				where: {
+					name: "general",
+				},
+				orderBy: {
+					createdAt: "asc",
+				},
+			},
+		},
+	})
+	// 找到服务器中的第一个通用频道 即 general
+	const initialChannel = server?.channels[0]
+
+	if (initialChannel?.name !== "general") {
+		return null
+	}
+	// 重定向到第一个通用频道 即 general
+	return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 
 export default ServerIdPage
