d01902f03f9209edefc129d8ebaa88d5b51ce633
苏征辉 <343196323@qq.com>
2024-06-22 22:24:01 +0800

    ✨ feat: 增加成员聊天界面顶部头像

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react"
 
 import { MobileToggle } from "@/components/mobile-toggle"
+import { UserAvatar } from "../user-avatar"
 
 interface ChatHeaderProps {
 	serverId: string
@@ -28,6 +29,10 @@ export const ChatHeader = ({
 			{/* 频道聊天的图标*/}
 			{type === "channel" && (
 				<Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />
+			)}
+			{/* 成员聊天的图标-即头像 */}
+			{type === "conversation" && (
+				<UserAvatar src={imageUrl} className='w-8 h-8 md:w-8 md:h-8 mr-2' />
 			)}
 			<p className='font-semibold text-md text-black dark:text-white'>{name}</p>
 		</div>
